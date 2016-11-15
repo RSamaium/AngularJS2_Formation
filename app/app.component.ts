20c00b808e8b65227c8d5493580c891a51890d2b
WebCreative5 <sam@webcreative5.net>
2016-11-15 09:15:17 +0100

    init

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,13 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-navbar></app-navbar>
-  <div class="col-sm-3 col-md-2 sidebar">
-    <app-menu></app-menu>
-  </div>
-  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <app-home></app-home>
-  </div>
+    <h1>{{title}}</h1>
   `
 })
 export class AppComponent {
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-import { NavbarModule } from './navbar/navbar.module';
-import { MenuModule } from './menu/menu.module';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    NavbarModule,
-    MenuModule,
-    HomeModule
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
