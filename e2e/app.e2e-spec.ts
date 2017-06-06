12a3f49bf90ae9dd327bc2a3abdbb1921cdf6224
Samarium <sam@webcreative5.net>
2017-06-06 21:25:58 +0200

    cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { CliPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('cli App', () => {
+  let page: CliPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new CliPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
