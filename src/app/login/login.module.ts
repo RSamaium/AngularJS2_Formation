b43d94ad1ed982b01988cba0788f3b26d914daf5
Samarium <sam@webcreative5.net>
2017-06-08 10:44:00 +0200

    angular cli

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { LoginComponent } from './login.component'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  declarations: [ LoginComponent ],
-  exports: [ LoginComponent ]
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
 })
 export class LoginModule { }
